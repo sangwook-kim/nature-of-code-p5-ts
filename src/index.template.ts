12ae4478a1b4b0edeaff9d41ef4f4b2ec6bd0413
galien <galien@galienui-MacBook-Pro.local>
2022-04-10 21:51:33 +0900

    Changing Movers functionally

--- src/index.template.ts
+++ src/index.template.ts
@@ -1,24 +1,15 @@
 import P5 from 'p5';
-import ConstantVMover from './ConstantVelocityMover';
 
 const sketch = (p5: P5) => {
     const width = 200;
     const height = 200;
-    const movers = ConstantVMover(p5, {width, height});
 
     p5.setup = () => {
         const canvas = p5.createCanvas(width, height);
         canvas.parent('p5app');
-
-        movers.create();
-        movers.create();
     };
 
-    p5.draw = () => {
-        p5.background(255, 16);
-
-        movers.update();
-    };
+    p5.draw = () => {};
 
     p5.mousePressed = () => {
         if (p5.isLooping()) {
