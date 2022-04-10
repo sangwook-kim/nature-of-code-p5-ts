781d1d2adb819462c5c45f476dd1a3b4bed80305
galien <galien@galienui-MacBook-Pro.local>
2022-04-10 21:59:53 +0900

    Add functional 1.7

---
+++ src/chapter01/ConstantVelocityMover.ts
@@ -0,0 +1,49 @@
+import P5 from 'p5';
+import Mover, {TMoverState} from './FunctionalMover';
+
+const ConstantVMover = (p5: P5, {width, height}) => {
+    const movers = Mover(p5);
+    const cMovers = [];
+
+    const create = () => {
+        const initialValue: TMoverState = {
+            location: p5.createVector(p5.random(0, width), p5.random(0, height)),
+            velocity: p5.createVector(p5.random(-2, 2), p5.random(-2, 2)),
+        };
+        cMovers.push(movers.useState(initialValue));
+    };
+
+    const handleEdges = (location: P5.Vector) => {
+        if (location.x > width) {
+            location.x = 0;
+        } else if (location.x < 0) {
+            location.x = width;
+        }
+
+        if (location.y > height) {
+            location.y = 0;
+        } else if (location.y < 0) {
+            location.y = height;
+        }
+    };
+
+    const update = () => {
+        cMovers.forEach((cMover) => {
+            const [moverState, setMoverState] = cMover;
+            const {location, velocity} = moverState;
+            const newLocation = location.add(velocity);
+            handleEdges(newLocation);
+
+            setMoverState({
+                location: newLocation,
+                velocity,
+            });
+        });
+
+        movers.display();
+    };
+
+    return {create, update};
+};
+
+export default ConstantVMover;
